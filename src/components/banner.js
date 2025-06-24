d7ab6681959676cf5789cabd4397210d2ed87fc2
Noppanut01 <noppanut.jang2547@gmail.com>
2025-06-24 23:30:39 +0700

    Create navbar and banner

---
+++ src/components/banner.js
@@ -0,0 +1,24 @@
+function Banner() {
+    return (
+        <div className="banner">
+            <p>Developer</p>
+            <div className="container">
+                <div>Hello, my name</div>
+                <div>is Noppanut Maiprot</div>
+            </div>
+            <br />
+
+            <div className="desc">
+                wall section printed automobile slowly powder youth explore cup duck bottom yellow know discover rise per equipment other round adjective settle shout pair bowl
+            </div>
+            <br />
+            <div className='button'>
+                <button className="project"><a href="https://github.com/Noppanut01" target="_blank">Projects</a></button>
+                <button className="github"><a href="https://github.com/Noppanut01" target="_blank">Github</a></button>
+            </div>
+        </div >
+    )
+}
+
+
+export default Banner 
---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+function Navbar() {
+    return (
+        <div className='navbar' >
+            <ul>
+                <li>Noppanut Maiprot</li>
+
+            </ul>
+            <ul>
+                <li>About</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </div >
+    );
+}
+
+export default Navbar;
